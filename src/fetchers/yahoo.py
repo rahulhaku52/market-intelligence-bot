024517df034479bad4457acff69c61b150f0974c
rahulhaku52 <152463758+rahulhaku52@users.noreply.github.com>
2026-07-27 22:11:51 +0530

    Update yahoo.py

--- src/fetchers/yahoo.py
+++ src/fetchers/yahoo.py
@@ -1,6 +1,7 @@
 import yfinance as yf
-def fetch(ticker, period="3mo"):
+
+def fetch(ticker, period="1mo", interval="1d"):
     stock = yf.Ticker(ticker)
-    hist = stock.history(period=period)
+    hist = stock.history(period=period, interval=interval)
     info = stock.info
     return hist, info
