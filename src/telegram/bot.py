872b4b3b40ab21e55af8a9b208e389291ada0020
rahulhaku52 <152463758+rahulhaku52@users.noreply.github.com>
2026-07-27 17:31:42 +0530

    Update bot.py

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -3,25 +3,18 @@ import time
 from telegram import Bot
 from telegram.error import TelegramError
 from tenacity import retry, stop_after_attempt, wait_exponential
-import re
-
-def escape_markdown(text):
-    # MarkdownV2 escape
-    escape_chars = r'_*[]()~`>#+-=|{}.!'
-    return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
 
 @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=10))
 def send_analysis(message, chart_bytes=None):
     bot = Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
     channel = os.environ['TELEGRAM_CHANNEL_ID']
-    safe_message = escape_markdown(message)
     try:
         if chart_bytes:
-            bot.send_photo(chat_id=channel, photo=chart_bytes, caption=safe_message, parse_mode='MarkdownV2')
+            bot.send_photo(chat_id=channel, photo=chart_bytes, caption=message, parse_mode='HTML')
         else:
-            bot.send_message(chat_id=channel, text=safe_message, parse_mode='MarkdownV2')
+            bot.send_message(chat_id=channel, text=message, parse_mode='HTML')
     except TelegramError as e:
         if 'Too Many Requests' in str(e):
             time.sleep(5)
-            raise  # retry
+            raise
         raise
