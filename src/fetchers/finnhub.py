829da913c3fb1ca5690602a9f563bc4467fe0750
rahulhaku52 <152463758+rahulhaku52@users.noreply.github.com>
2026-07-27 14:16:50 +0530

    Create finnhub.py

---
+++ src/fetchers/finnhub.py
@@ -0,0 +1,11 @@
+import os
+import finnhub
+
+def get_client():
+    return finnhub.Client(api_key=os.environ['FINNHUB_API_KEY'])
+
+def fetch_news(ticker, from_date, to_date):
+    return get_client().company_news(ticker, _from=from_date, to=to_date)
+
+def fetch_quote(ticker):
+    return get_client().quote(ticker)
